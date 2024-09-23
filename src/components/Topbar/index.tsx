dadbeb5cb7f46436b65b1e92aba962aeae1a7a11
Babatunde Ojo <babatundeojo30@gmail.com>
2024-09-23 13:43:37 +0100

    add padding to top bar

--- src/components/Topbar/index.tsx
+++ src/components/Topbar/index.tsx
@@ -3,7 +3,7 @@ import { getCurrentDate } from "@/utils/helper";
 
 const TopBar = () => {
   return (
-    <header className="flex justify-between p-4 bg-gray-50">
+    <header className="flex justify-between px-8 py-6 bg-gray-50">
       <div className="flex flex-col">
         <h1 className="font-averta text-2xl text-[#15192C]">Dashboard</h1>
         <p className="text-[#92959E] text-xs mt-1">Information about your current plan and usages</p>
